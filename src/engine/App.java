48f559ed42b84e9d6168441d7e009f66b88d042b
Marsu2 <elouanbarbier296@gmail.com>
2025-12-08 20:07:28 +0100

    fezezfzf

--- src/engine/App.java
+++ src/engine/App.java
@@ -11,6 +11,8 @@ import game.Game;
 public class App {
     public static void main(String[] args) throws Exception {
         // Création d'un nouveau jeu et lancement de celui-ci
+        // javadoc -d ../docs -sourcepath . -subpackages engine:game
+        // Ligne javadoc pour faire la docu a bien faire dans src
         Game g = new Game();
         g.launch();
     }
